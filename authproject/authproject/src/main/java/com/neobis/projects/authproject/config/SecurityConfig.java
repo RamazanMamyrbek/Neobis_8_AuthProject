69a8edb6c375ff87712758151984d704d77ff09f
RamazanMamyrbek <rama.mamirbek@gmail.com>
2024-04-29 23:18:34 +0500

    Added functionality: Login with httpBasic

--- authproject/authproject/src/main/java/com/neobis/projects/authproject/config/SecurityConfig.java
+++ authproject/authproject/src/main/java/com/neobis/projects/authproject/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,12 +20,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(
                 request -> request
                         .requestMatchers("/api/register", "/swagger-ui/**").permitAll()
                         .requestMatchers("/api/me", "/api/logout").authenticated()
                         .anyRequest().permitAll()
         );
+        http.httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
